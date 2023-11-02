67799133683fd622c10860e8766042236eca23fd
Matt Lewis <hello@mattlewis.me>
2023-11-02 19:50:19 +0000

    feat: scaffold out a demo application

---
+++ apps/demo/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NxWelcomeComponent } from './nx-welcome.component';
+
+@Component({
+  standalone: true,
+  imports: [NxWelcomeComponent],
+  selector: 'clickup-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'demo';
+}
